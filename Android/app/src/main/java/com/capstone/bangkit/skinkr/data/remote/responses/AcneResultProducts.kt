cb9214e04974abe098ed4c5f4740d3cdb53621fa
imrbp <imr.bp.24@gmail.com>
2022-06-12 11:25:07 +0700

    Done? i think

--- Android/app/src/main/java/com/capstone/bangkit/skinkr/data/remote/responses/AcneResultProducts.kt
+++
@@ -1,8 +0,0 @@
-package com.capstone.bangkit.skinkr.data.remote.responses
-
-import com.google.gson.annotations.SerializedName
-
-data class AcneResultProducts(
-    @SerializedName("")
-    val productName: String
-)
--- Android/app/src/main/java/com/capstone/bangkit/skinkr/presentation/result/ResultViewModel.kt
+++
@@ -1,4 +0,0 @@
-package com.capstone.bangkit.skinkr.presentation.result
-
-class ResultViewModel {
-}
